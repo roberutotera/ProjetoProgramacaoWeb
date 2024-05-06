79e5a66f3f661f6c6b1acbac7806c7a440092b2a
Roberto Záccaro <136395395+roberutotera@users.noreply.github.com>
2024-05-06 17:02:29 -0300

    Delete Curso directory

--- Curso/src/main/java/com/jogos/roberto/curso/empresa/DadosListagemEmpresa.java
+++
@@ -1,9 +0,0 @@
-package com.jogos.roberto.curso.empresa;
-
-public record DadosListagemEmpresa(Long id,String nome_empresa) {
-	
-	public DadosListagemEmpresa (Empresa empresa) {
-		this(empresa.getId(), empresa.getNome_empresa());
-	}
-
-}
